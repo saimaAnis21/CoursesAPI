bd7ef2b33203e6f5f66a8b765fea9238213e55d9
saimaAnis21 <saima_rehman21@yahoo.com>
2021-08-05 20:14:55 +0500

    Fix response.rb

--- app/controllers/concerns/response.rb
+++ app/controllers/concerns/response.rb
@@ -1,6 +1,6 @@
 # app/controllers/concerns/response.rb
 module Response
   def json_response(object, status = :ok)
-    render json: object, status: status, response_headers: 
+    render json: object, status: status
   end
 end
