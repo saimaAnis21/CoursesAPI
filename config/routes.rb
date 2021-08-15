d277794e91829326d97ab5ed90d693867219c3ed
saimaAnis21 <saima_rehman21@yahoo.com>
2021-08-15 16:04:32 +0500

    Change authentication route and add uniqueness to email in DB

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :courses
   resources :category_names , only: :index
-  post 'auth/login', to: 'authentication#authenticate'
+  post 'authentication', to: 'authentication#create'
   post 'signup', to: 'users#create'
 end
